3d217a35aec50c56f5e384b698fef96a412fe5b9
anitarathod <92431097+anitarathod@users.noreply.github.com>
2021-11-11 16:58:31 +0530

    square
    square of a number

---
+++ jquery.square.js
@@ -0,0 +1,13 @@
+(function ( $ ) {
+ 
+    $.fn.square = function( options ) {
+        let value = parseInt($(this).val());
+        return  value * value ;    
+    };
+  
+    
+  }( jQuery ));
+
+
+
+
